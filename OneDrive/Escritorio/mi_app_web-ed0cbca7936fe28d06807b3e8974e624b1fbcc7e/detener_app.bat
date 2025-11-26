f78080850815b2d3bd0ec150615e4f577dfcd94b
Tu Nombre <tu.email@ejemplo.com>
2025-11-26 01:51:49 -0400

    Limpiar repositorio: eliminar archivos de proyectos anteriores y archivos con caracteres especiales

--- OneDrive/Escritorio/mi_app_web-ed0cbca7936fe28d06807b3e8974e624b1fbcc7e/detener_app.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-echo Deteniendo el sistema...
-taskkill /F /IM python.exe
-echo Sistema detenido.
-pause 
--- OneDrive/Escritorio/mi_app_web-ed0cbca7936fe28d06807b3e8974e624b1fbcc7e/iniciar_app.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-echo Iniciando el sistema...
-cd /d "%~dp0"
-call .\venv\Scripts\activate
-python app.py
-pause 
